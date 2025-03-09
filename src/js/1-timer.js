237dfd5ac5cc666987499e154fb7cf64822142b8
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-03-10 00:35:32 +0200

    ferst-part-with-flatpickr

---
+++ src/js/1-timer.js
@@ -0,0 +1,42 @@
+// console.log('Скрипт 1-timer.js працює!');
+// document.body.style.backgroundColor;
+const inputEl = document.querySelector('#datetime-picker');
+const Btn = document.querySelector('button');
+
+const valueEl = document.querySelector('.value');
+
+class Timer {
+  constructor({ onTick }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+
+    this.init();
+  }
+
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    const startTime = Date.now();
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000);
+  }
+}
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
