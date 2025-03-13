32d684abf3a960133118f225b1b323b3c202cee4
OlenaIvaschenko <olenaivaschenko@ukr.net>
2025-03-13 21:39:23 +0000

    Deploying to gh-pages from @ OlenaIvaschenko/goit-js-hw-10@d09f9054f7462702ca03c4dd8086d39c89fe9b88 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-Cu5NhK-P.js";import"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".form");t.setAttribute("novalidate","");
+//# sourceMappingURL=2-snackbar.js.map
